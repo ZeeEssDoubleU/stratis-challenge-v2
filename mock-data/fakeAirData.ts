b440744368594cd9cd9e9102fb7889b5867b9006
Zak Williams <zak.williams2287@gmail.com>
2021-05-23 13:54:43 -0400

    data:  added mock api data

---
+++ mock-data/fakeAirData.ts
@@ -0,0 +1,222 @@
+export const data = {
+	data: {
+		aqi: 46,
+		attributions: [
+			{
+				logo: "USA-CAARB.png",
+				name: "CARB - California Air Resources Board",
+				url: "http://www.arb.ca.gov/",
+			},
+			{
+				name: "Air Now - US EPA",
+				url: "http://www.airnow.gov/",
+			},
+			{
+				name: "World Air Quality Index Project",
+				url: "https://waqi.info/",
+			},
+		],
+		city: {
+			geo: [37.76595, -122.39902],
+			name: "San Francisco-Arkansas Street, San Francisco, California",
+			url: "https://aqicn.org/city/california/san-francisco/san-francisco-arkansas-street",
+		},
+		debug: {
+			sync: "2021-05-23T13:09:17+09:00",
+		},
+		dominentpol: "pm25",
+		forecast: {
+			daily: {
+				o3: [
+					{
+						avg: 35,
+						day: "2021-05-20",
+						max: 41,
+						min: 32,
+					},
+					{
+						avg: 29,
+						day: "2021-05-21",
+						max: 38,
+						min: 26,
+					},
+					{
+						avg: 30,
+						day: "2021-05-22",
+						max: 41,
+						min: 20,
+					},
+					{
+						avg: 34,
+						day: "2021-05-23",
+						max: 42,
+						min: 26,
+					},
+					{
+						avg: 23,
+						day: "2021-05-24",
+						max: 44,
+						min: 14,
+					},
+					{
+						avg: 29,
+						day: "2021-05-25",
+						max: 41,
+						min: 24,
+					},
+				],
+				pm10: [
+					{
+						avg: 4,
+						day: "2021-05-20",
+						max: 6,
+						min: 2,
+					},
+					{
+						avg: 7,
+						day: "2021-05-21",
+						max: 10,
+						min: 4,
+					},
+					{
+						avg: 10,
+						day: "2021-05-22",
+						max: 11,
+						min: 8,
+					},
+					{
+						avg: 7,
+						day: "2021-05-23",
+						max: 11,
+						min: 4,
+					},
+					{
+						avg: 7,
+						day: "2021-05-24",
+						max: 12,
+						min: 4,
+					},
+					{
+						avg: 4,
+						day: "2021-05-25",
+						max: 8,
+						min: 3,
+					},
+				],
+				pm25: [
+					{
+						avg: 6,
+						day: "2021-05-20",
+						max: 9,
+						min: 3,
+					},
+					{
+						avg: 12,
+						day: "2021-05-21",
+						max: 19,
+						min: 7,
+					},
+					{
+						avg: 19,
+						day: "2021-05-22",
+						max: 23,
+						min: 17,
+					},
+					{
+						avg: 12,
+						day: "2021-05-23",
+						max: 18,
+						min: 8,
+					},
+					{
+						avg: 15,
+						day: "2021-05-24",
+						max: 36,
+						min: 6,
+					},
+					{
+						avg: 9,
+						day: "2021-05-25",
+						max: 11,
+						min: 4,
+					},
+				],
+				uvi: [
+					{
+						avg: 0,
+						day: "2021-05-21",
+						max: 1,
+						min: 0,
+					},
+					{
+						avg: 1,
+						day: "2021-05-22",
+						max: 6,
+						min: 0,
+					},
+					{
+						avg: 1,
+						day: "2021-05-23",
+						max: 7,
+						min: 0,
+					},
+					{
+						avg: 1,
+						day: "2021-05-24",
+						max: 9,
+						min: 0,
+					},
+					{
+						avg: 1,
+						day: "2021-05-25",
+						max: 9,
+						min: 0,
+					},
+					{
+						avg: 2,
+						day: "2021-05-26",
+						max: 9,
+						min: 0,
+					},
+				],
+			},
+		},
+		iaqi: {
+			co: {
+				v: 1.7,
+			},
+			h: {
+				v: 85.2,
+			},
+			no2: {
+				v: 2.8,
+			},
+			o3: {
+				v: 31.2,
+			},
+			p: {
+				v: 1019.4,
+			},
+			pm25: {
+				v: 46,
+			},
+			t: {
+				v: 11.2,
+			},
+			w: {
+				v: 2.8,
+			},
+			wg: {
+				v: 10.1,
+			},
+		},
+		idx: 3900,
+		time: {
+			iso: "2021-05-22T19:00:00-07:00",
+			s: "2021-05-22 19:00:00",
+			tz: "-07:00",
+			v: 1621710000,
+		},
+	},
+	status: "ok",
+}
